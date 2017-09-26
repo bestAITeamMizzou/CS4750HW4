f33da0a686704f4cf3aeae1f69ef45dbe631cf0e
Peter <p.eckelmann@gmail.com>
2017-09-26 11:20:29 -0500

    *Untested* Added methods for finding 2 & 3 in a row

--- CS4750HW4/Form1.cs
+++ CS4750HW4/Form1.cs
@@ -17,6 +17,19 @@ namespace CS4750HW4
         X = 1
     } //End public enum BoardVals
 
+    public enum BoardDirection
+    {
+        NUll = -1,
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3,
+        UpLeft = 4,
+        UpRight = 5,
+        DownLeft = 6,
+        DownRight = 7
+    } //End public enum BoardDirection
+
     public partial class Form1 : Form
     {
         /***************ATTRIBUTES***************/
--- CS4750HW4/GameBoard.cs
+++ CS4750HW4/GameBoard.cs
@@ -66,6 +66,22 @@ namespace CS4750HW4
             return returnVal;
         } //End private bool isValidSpace(Point tile)
 
+        private bool isValidSpace(Point tileToConsider, BoardVals valToConsider)
+        {
+            //Declare variables
+            bool returnVal = false;
+
+            if ((tileToConsider.X >= 0 && tileToConsider.X < 5) && (tileToConsider.Y >= 0 && tileToConsider.Y < 6))
+            {
+                if (this.Board[tileToConsider.X, tileToConsider.Y] == valToConsider)
+                {
+                    returnVal = true;
+                } //End if (this.Board[tileToConsider.X, tileToConsider.Y] == valToConsider)
+            } //End if ((tileToConsider.X >= 0 && tileToConsider.X < 5) && (tileToConsider.Y >= 0 && tileToConsider.Y < 6))
+
+            return returnVal;
+        } //End private bool isValidSpace(Point tileToConsider, BoardVals valToConsider)
+
         public List<Point> getPossibleMoves()
         {
             //Declare variables
@@ -85,66 +101,129 @@ namespace CS4750HW4
             return possibleMoves;
         } //End public List<Point> getPossibleMoves()
 
-        private List<Point> getValidSurroundingTiles(Point tileToConsider)
+        private List<Point> getValidSurroundingTiles(Point tileToConsider, BoardVals valToConsider)
         {
             //Declare variables
             List<Point> validTiles = new List<Point>();
 
             //Up
-            if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1)))
+            if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X, tileToConsider.Y - 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1), valToConsider))
 
             //Down
-            if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1)))
+            if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X, tileToConsider.Y + 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1), valToConsider))
 
             //Left
-            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y)))
+            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X - 1, tileToConsider.Y));
-            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y)))
+            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y), valToConsider))
 
             //Right
-            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y)))
+            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X + 1, tileToConsider.Y));
-            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y)))
+            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y), valToConsider))
 
             //Up-Left
-            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1)))
+            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X - 1, tileToConsider.Y - 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1), valToConsider))
 
             //Up-Right
-            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1)))
+            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X + 1, tileToConsider.Y - 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1), valToConsider))
 
             //Down-Left
-            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1)))
+            if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X - 1, tileToConsider.Y + 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1), valToConsider))
 
             //Down-Right
-            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1)))
+            if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1), valToConsider))
             {
                 validTiles.Add(new Point(tileToConsider.X + 1, tileToConsider.Y + 1));
-            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1)))
+            } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1), valToConsider))
 
             return validTiles;
-        } //End private List<Point> getValidSurroundingTiles(Point tileToConsider)
+        } //End private List<Point> getValidSurroundingTiles(Point tileToConsider, BoardVals valToConsider)
+
+        private Point getPossibleThirdInARow(Point tileToConsider, BoardVals valToConsider, BoardDirection direction)
+        {
+            //Declare variables
+            Point validTile = new Point (-1, -1);
+
+            switch (direction)
+            {
+                case BoardDirection.Up:
+                    if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X, tileToConsider.Y - 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y - 1), valToConsider))
+                    break;
+                case BoardDirection.Down:
+                    if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X, tileToConsider.Y + 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X, tileToConsider.Y + 1), valToConsider))
+                    break;
+                case BoardDirection.Left:
+                    if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X - 1, tileToConsider.Y);
+                    } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y), valToConsider))
+                    break;
+                case BoardDirection.Right:
+                    if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X + 1, tileToConsider.Y);
+                    } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y), valToConsider))
+                    break;
+                case BoardDirection.UpLeft:
+                    if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X - 1, tileToConsider.Y - 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y - 1), valToConsider))
+                    break;
+                case BoardDirection.UpRight:
+                    if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X + 1, tileToConsider.Y - 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y - 1), valToConsider))
+                    break;
+                case BoardDirection.DownLeft:
+                    if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X - 1, tileToConsider.Y + 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X - 1, tileToConsider.Y + 1), valToConsider))
+                    break;
+                case BoardDirection.DownRight:
+                    if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1), valToConsider))
+                    {
+                        validTile = new Point(tileToConsider.X + 1, tileToConsider.Y + 1);
+                    } //End if (isValidSpace(new Point(tileToConsider.X + 1, tileToConsider.Y + 1), valToConsider))
+                    break;
+                default:
+                    break;
+            } //End switch (direction)
+
+            return validTile;
+        } //End private Point getPossibleThirdInARow(Point tileToConsider, BoardVals valToConsider, BoardDirection direction)
 
         public List<List<Point>> getTwosInARow(BoardVals valToConsider)
         {
             //Declare variables
             List<List<Point>> twos = new List<List<Point>>();
+            List<Point> possible2nds = new List<Point>();
 
             for (int j = 0; j < 6; j++)
             {
@@ -152,8 +231,18 @@ namespace CS4750HW4
                 {
                     if (this.Board[i,j] == valToConsider)
                     {
-
-                    } //End 
+                        possible2nds = getValidSurroundingTiles(new Point(i,j), valToConsider);
+                        if (possible2nds.Count > 0)
+                        {
+                            for (int x = 0; x < possible2nds.Count; x++)
+                            {
+                                List<Point> temp = new List<Point>();
+                                temp.Add(new Point(i, j));
+                                temp.Add(possible2nds[x]);
+                                twos.Add(temp);
+                            } //End for (int x = 0; x < possible2nds.Count; x++)
+                        } //End if (possible2nds.Count > 0)
+                    } //End if (this.Board[i,j] == valToConsider)
                 } //End for (int i = 0; i < 5; i++)
             } //End for (int j = 0; j < 6; j++)
 
@@ -164,9 +253,94 @@ namespace CS4750HW4
         {
             //Declare variables
             List<List<Point>> threes = new List<List<Point>>();
+            List<Point> possible2nds = new List<Point>();
+            Point possible3rd;
+
+            for (int j = 0; j < 6; j++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (this.Board[i, j] == valToConsider)
+                    {
+                        possible2nds = getValidSurroundingTiles(new Point(i, j), valToConsider);
+                        if (possible2nds.Count > 0)
+                        {
+                            for (int x = 0; x < possible2nds.Count; x++)
+                            {
+                                possible3rd = getPossibleThirdInARow(possible2nds[x], valToConsider, determineDirectionT1ToT2(new Point(i, j), possible2nds[x]));
+                                if (isValidSpace(possible3rd, valToConsider))
+                                {
+                                    List<Point> temp = new List<Point>();
+                                    temp.Add(new Point(i, j));
+                                    temp.Add(possible2nds[x]);
+                                    temp.Add(possible3rd);
+                                    threes.Add(temp);
+                                } //End if (isValidSpace(possible3rd, valToConsider))
+                            } //End for (int x = 0; x < possible2nds.Count; x++)
+                        } //End if (possible2nds.Count > 0)
+                    } //End if (this.Board[i,j] == valToConsider)
+                } //End for (int i = 0; i < 5; i++)
+            } //End for (int j = 0; j < 6; j++)
 
             return threes;
         } //End public List<List<Point>> getThreesInARow(BoardVals valToConsider)
+
+        private BoardDirection determineDirectionT1ToT2(Point tile1, Point tile2)
+        {
+            //Declare variables
+            BoardDirection returnVal = BoardDirection.NUll;
+
+            if (isValidSpace(tile1) && isValidSpace(tile2))
+            {
+                if (tile1.X > tile2.X)
+                {
+                    if (tile1.Y > tile2.Y)
+                    {
+                        returnVal = BoardDirection.DownRight;
+                    } //End if (tile1.Y > tile2.Y)
+                    else if (tile1.Y < tile2.Y)
+                    {
+                        returnVal = BoardDirection.UpRight;
+                    } //End else if (tile1.Y < tile2.Y)
+                    else
+                    {
+                        returnVal = BoardDirection.Right;
+                    } //End else
+                } //End if (tile1.X > tile2.X)
+                else if (tile1.X < tile2.X)
+                {
+                    if (tile1.Y > tile2.Y)
+                    {
+                        returnVal = BoardDirection.DownLeft;
+                    } //End if (tile1.Y > tile2.Y)
+                    else if (tile1.Y < tile2.Y)
+                    {
+                        returnVal = BoardDirection.UpLeft;
+                    } //End else if (tile1.Y < tile2.Y)
+                    else
+                    {
+                        returnVal = BoardDirection.Left;
+                    } //End else
+                } //End else if (tile1.X < tile2.X)
+                else
+                {
+                    if (tile1.Y > tile2.Y)
+                    {
+                        returnVal = BoardDirection.Down;
+                    } //End if (tile1.Y > tile2.Y)
+                    else if (tile1.Y < tile2.Y)
+                    {
+                        returnVal = BoardDirection.Up;
+                    } //End else if (tile1.Y < tile2.Y)
+                    else
+                    {
+                        returnVal = BoardDirection.NUll;
+                    } //End else
+                } //End else
+            } //End if (isValidSpace(tile1) && isValidSpace(tile2))
+
+            return returnVal;
+        } //End private BoardDirection determineDirectionT1ToT2(Point tile1, Point tile2)
 
     } //End class Board
 } //End namespace CS4750HW4
